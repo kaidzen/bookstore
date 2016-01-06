10ca7bd5bf3a0f1bdf267d88d34029dae45e72c7
Kaidzen <karpov.srg@gmail.com>
2016-01-06 17:06:48 +0200

    Added: domain - Book

--- src/main/java/org/hardcrystal/bookmng/domain/Book.java
+++ src/main/java/org/hardcrystal/bookmng/domain/Book.java
@@ -1,7 +1,42 @@
 package org.hardcrystal.bookmng.domain;
 
-/**
- * Created by Administrator on 05.01.2016.
- */
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "books")
 public class Book {
+
+    @Id
+    @Column
+    @GeneratedValue
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "genre")
+    private String genre;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
